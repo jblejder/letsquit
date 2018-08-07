5a96ab0a234089c09a05d91f90d1704ebad82c30
jblejder <jakubblejder@gmail.com>
2018-08-07 23:16:38 +0200

    connect activities

---
+++ app/src/main/java/com/projectblejder/letsquit/MyHabit.kt
@@ -0,0 +1,28 @@
+package com.projectblejder.letsquit
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class MyHabit(val context: Context) {
+
+    private val prefs: SharedPreferences
+        get() = context.getSharedPreferences("user-data", Context.MODE_PRIVATE)
+
+
+    var habit: Habit?
+        get() = getMyHabit()
+        set(value) = setMyHabit(value)
+
+    private fun setMyHabit(value: Habit?) {
+        prefs.edit().also {
+            it.putString("my-habit", value?.name)
+            it.apply()
+        }
+
+    }
+
+    private fun getMyHabit(): Habit? {
+        return prefs.getString("my-habit", null)?.let { Habit(it) }
+    }
+
+}
--- app/src/main/java/com/projectblejder/letsquit/NameBadHabitActivity.kt
+++ app/src/main/java/com/projectblejder/letsquit/NameBadHabitActivity.kt
@@ -1,12 +1,21 @@
 package com.projectblejder.letsquit
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.projectblejder.letsquit.databinding.NameBadHabitActivityBinding
 
 class NameBadHabitActivity : BaseActivity() {
 
+    lateinit var binding: NameBadHabitActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.name_bad_habit_activity)
+
+        binding.getInspiredButtom.setOnClickListener {
+            startActivity(Intent(this, SelectPredefinedBadHabitsActivity::class.java))
+        }
     }
 }
 
