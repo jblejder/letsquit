af0f9bcd3ee88224383df577baa7afb6c7b74e56
jblejder <jakubblejder@gmail.com>
2018-08-11 21:03:36 +0200

    add missing functionality to name habit activity

--- app/src/main/java/com/projectblejder/letsquit/CounterActivity.kt
+++ app/src/main/java/com/projectblejder/letsquit/CounterActivity.kt
@@ -2,13 +2,13 @@ package com.projectblejder.letsquit
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import com.projectblejder.letsquit.databinding.IntroductionActivityBinding
+import com.projectblejder.letsquit.databinding.CounterActivityBinding
 import com.projectblejder.letsquit.shared.BaseActivity
 import com.projectblejder.letsquit.shared.MyHabit
 
 class CounterActivity : BaseActivity() {
 
-    lateinit var binding: IntroductionActivityBinding
+    lateinit var binding: CounterActivityBinding
 
     lateinit var counter: Counter
 
@@ -18,12 +18,12 @@ class CounterActivity : BaseActivity() {
 
         counter = Counter(this)
 
-        binding.title.text = MyHabit(this).habit?.name ?: "No habit :("
-        binding.description.text = "${counter.number}"
+        binding.habitName.text = MyHabit(this).habit?.name ?: "No habit :("
+        binding.counter.text = "${counter.number}"
 
         binding.root.setOnClickListener {
             counter.number = counter.number + 1
-            binding.description.text = "${counter.number}"
+            binding.counter.text = "${counter.number}"
         }
     }
 }
--- app/src/main/java/com/projectblejder/letsquit/habitSelection/NameBadHabitActivity.kt
+++ app/src/main/java/com/projectblejder/letsquit/habitSelection/NameBadHabitActivity.kt
@@ -3,9 +3,13 @@ package com.projectblejder.letsquit.habitSelection
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import com.projectblejder.letsquit.shared.BaseActivity
+import com.projectblejder.letsquit.CounterActivity
 import com.projectblejder.letsquit.R
 import com.projectblejder.letsquit.databinding.NameBadHabitActivityBinding
+import com.projectblejder.letsquit.shared.BaseActivity
+import com.projectblejder.letsquit.shared.Habit
+import com.projectblejder.letsquit.shared.MyHabit
+import org.jetbrains.anko.startActivity
 
 class NameBadHabitActivity : BaseActivity() {
 
@@ -17,6 +21,14 @@ class NameBadHabitActivity : BaseActivity() {
 
         binding.getInspiredButtom.setOnClickListener {
             startActivity(Intent(this, SelectPredefinedBadHabitsActivity::class.java))
+        }
+
+        binding.startButton.setOnClickListener {
+            val habitName = binding.inputText.text?.toString() ?: ""
+            if (habitName.length > 0) {
+                MyHabit(this).habit = Habit(habitName)
+                startActivity<CounterActivity>()
+            }
         }
     }
 }
